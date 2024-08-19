280e7547f93cdc5db5ef0e20a85a23c62e6c15fd
Nobert-Muma <mumanobert17@gmail.com>
2024-08-19 09:52:45 +0300

    Wrote code to pass the tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Nobert Muma</h1>
+      <img src="https://images.unsplash.com/photo-1664331744656-709fdc75328d?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="A picture of Nobert at work"/>
+      <h2>About Me</h2>
+      <p>Count on me to get your work done.</p>
+      <button><a href="https://github.com/Nobert-Muma">GitHub</a></button>
+      <button><a href="https://www.linkedin.com/in/nobert-muma-46a58b312/">LinkedIn</a></button>
+    </div>
+  );
 }
 
 export default App;
